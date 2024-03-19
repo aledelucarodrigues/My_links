c149ff0116625afe4caba731a568a4c1d8827587
Alessandra de Luca Rodrigues <alessandralp1@yahoo.com.br>
2024-03-19 10:04:13 -0300

    Alterações Imagens Avatar

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const img = document.querySelector("#profile img")
 // substituir a imagem
 if (html.classList.contains("light")) {
 // se tiver light mode adicionar imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar.png")
 } else{
 // se tiver sem light mode manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
